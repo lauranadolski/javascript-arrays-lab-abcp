92a09a81a5005885ea0fc5bf158e46a25d005500
Laura Nadolski <laura.nadolski@gmail.com>
2018-01-15 01:36:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const app = "I don't do much.";
 
 var kittens = ["Milo", "Otis", "Garfield"];
 
-//
+//appends a kitten to the end of the kittens array
 function destructivelyAppendKitten(name){
   
 }
+
+//
